b4b50f41edcf27b210eeef5cae3bf798b23e73c0
Ashmin Jayson <ashminjayson10@gmail.com>
2022-12-14 20:32:15 +0530

    Added Survery Machine

---
+++ etlabsurveybot.py
@@ -0,0 +1,17 @@
+import pyautogui as pg
+import time
+
+time.sleep(7)
+
+for i in range(14):
+    pg.press('tab')
+
+for i in range(11):
+    pg.press('tab')
+    pg.press('down')
+    pg.press('up')
+
+for i in range(3):
+    pg.press('tab')
+
+pg.press('enter')
